4a3e3488b619d7cfa3492173f7dee892325b5e29
okay146 <oyuka6141@gmail.com>
2024-09-01 18:08:00 +0900

    firebase作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Report from './pages/Report';
+import NoMatch from './pages/NoMatch';
+import AppLayout from './components/layout/AppLayout';
+import { theme } from './theme/theme';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
 
 function App() {
+  // 取得したデータを配列に入れる
+  const [ transactions, setTransactions ] = useState([]);
   return (
+    <ThemeProvider theme={theme}>
+    {/* リセットCSSのようなもの */}
+    <CssBaseline />
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<Report />} />
+        <Route path='/' element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/report" element={<Report />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
       </Routes>
     </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export type TransactionType = "income" | "expense";
+export type incomeCategory = "給与" | "副収入" | "お小遣い";
+export type expenseCategory = "食費" | "日用品" | "住居費" | "交際費" | "娯楽"| "交通費";
+
+
+export interface Transition {
+    id: string;
+    date: string;
+    amount: number;
+    content: string;
+    type: TransactionType;
+    category: incomeCategory | expenseCategory;
+}
