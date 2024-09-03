e25f149d46a5aabbc37bf9d01357a9c7aaabfe06
okay146 <oyuka6141@gmail.com>
2024-09-03 22:47:43 +0900

    カレンダー機能の追加

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -2,11 +2,29 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import jaLocale from '@fullcalendar/core/locales/ja'
 import "../calendar.css"
+import { EventContentArg } from '@fullcalendar/core';
 
 const Calender = () => {
     const events = [
         { title: 'Meeting', start: new Date() },
     ]; 
+    const renderEventContnt = (eventInfo: EventContentArg) => {
+        return (
+            <div>
+                <div className='money' id='event-income'>
+                    {eventInfo.event.extendedProps.income}
+                </div>
+
+                <div className='money' id='event-expense'>
+                    {eventInfo.event.extendedProps.expense}
+                </div>
+
+                <div className='money' id='event-balance'>
+                    {eventInfo.event.extendedProps.balance}
+                </div>
+            </div>
+        );
+    };
 
     return (
         <FullCalendar 
@@ -14,6 +32,7 @@ const Calender = () => {
             initialView='dayGridMonth'
             locale={jaLocale}
             events={events}
+            eventContent={renderEventContnt}
         />
     );
 };
