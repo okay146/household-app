eaec701ea2d09885ab711cfcb87090b84b1c8111
okay146 <oyuka6141@gmail.com>
2024-09-05 22:34:46 +0900

    日付データを代入

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,31 +1,34 @@
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import jaLocale from '@fullcalendar/core/locales/ja'
-import "../calendar.css"
+import jaLocale from '@fullcalendar/core/locales/ja';
+import "../calendar.css";
 import { EventContentArg } from '@fullcalendar/core';
 
 const Calender = () => {
     const events = [
-        { title: 'Meeting', start: new Date() },
+        { title: 'Meeting', start: "2024-09-16", income: 300, expense: 200, balance: 100 },
     ]; 
+
     const renderEventContent = (eventInfo: EventContentArg) => {
         console.log(eventInfo);
+        console.log(eventInfo.event.extendedProps);
         return (
             <div>
-                <div className='money' id='event-income'>
+                <div className="money" id="event-income">
                     {eventInfo.event.extendedProps.income}
                 </div>
 
-                <div className='money' id='event-expense'>
+                <div className="money" id="event-expense">
                     {eventInfo.event.extendedProps.expense}
                 </div>
 
-                <div className='money' id='event-balance'>
+                <div className="money" id="event-balance">
                     {eventInfo.event.extendedProps.balance}
                 </div>
             </div>
         );
     };
+
 
     return (
         <FullCalendar 
