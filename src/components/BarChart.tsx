969e562f89df5a2c4ba162832a07e0ab44f86ca6
okay146 <oyuka6141@gmail.com>
2024-10-22 22:00:08 +0900

    ダミーデータでグラフ表示

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,8 +1,64 @@
+import { display } from '@mui/system';
+import { plugins } from 'chart.js';
 import React from 'react'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
 const BarChart = () => {
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: "top" as const,
+            },
+            title: {
+                display: true,
+                text: "Chart.js Bar Chart",
+            },
+        },
+    };
+    const labels = [
+        "2024-01-10",
+        "2024-01-11",
+        "2024-01-12",
+        "2024-01-13",
+        "2024-01-14",
+        "2024-01-15",
+    ];
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: "収入",
+                data: [100, 200, 500, 1000, 250, 300],
+                backgroundColor: "rgba(255, 99, 132, 0.5)",
+            },
+            {
+                label: "支出",
+                data: [1000, 20, 570, 1000, 2900, 100],
+                backgroundColor: "rgba(53, 162, 235, 0.5)",
+            },
+        ],
+    };
+
     return (
-        <div>BarChart</div>
+        <Bar options={options} data={data} />
     )
 };
 
