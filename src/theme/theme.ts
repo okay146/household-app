82749198b57fac64db07f213e2a45d6779078b8d
okay146 <oyuka6141@gmail.com>
2024-12-18 22:22:14 +0900

    色の追加

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,5 +1,6 @@
 import { createTheme, PaletteColor, PaletteColorOptions } from "@mui/material";
-import { blue, green, red } from "@mui/material/colors";
+import { amber, blue, blueGrey, cyan, deepOrange, green, lightBlue, lightGreen, lime, pink, purple, red, yellow } from "@mui/material/colors";
+import { ExpenseCategory, IncomeCategory } from "../types";
 
 // カスタムカラーを作る場合型を拡張する！
 declare module "@mui/material/styles" {
@@ -7,6 +8,8 @@ declare module "@mui/material/styles" {
         incomeColor: PaletteColor;
         expenseColor: PaletteColor;
         balanceColor: PaletteColor;
+        incomeCategoryColor: Record<IncomeCategory, string>;
+        expenseCategoryColor: Record<ExpenseCategory, string>;
     }
     // 初期カラー
     // ?をつけることで初期カラーが設定されていなくてもOKになる
@@ -14,6 +17,8 @@ declare module "@mui/material/styles" {
         incomeColor?: PaletteColorOptions;
         expenseColor?: PaletteColorOptions;
         balanceColor?: PaletteColorOptions;
+        incomeCategoryColor?: Record<IncomeCategory, string>;
+        expenseCategoryColor?: Record<ExpenseCategory, string>;
     }
 
 }
@@ -45,5 +50,25 @@ export const theme = createTheme({
             light: green[100],
             dark: green[700],
         },
+        // 収入用円
+        incomeCategoryColor : {
+            給与: lightBlue[500],
+            副収入: cyan[200],
+            お小遣い: lightGreen['A700'],
+        },
+        // 支出用
+        expenseCategoryColor : {
+            食費: deepOrange[500],
+            日用品: lightGreen[500],
+            住居費: amber[500],
+            交際費: pink[300],
+            娯楽: cyan[200],
+            交通費: purple[400],
+            病院: lime[500],
+            コンビニ: yellow[500],
+            その他: blueGrey[500],
+        }
+
+
     }
 });
